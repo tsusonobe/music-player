3ad44062289dffa4ecd3e0aaa8103cca8a4c3fbb
tsusonobe <sonobe.tsu@gmail.com>
2024-08-30 12:20:15 -0300

    organizando os arquivos na pasta src

--- utils.js
+++
@@ -1,7 +0,0 @@
-const secondsToMinutes = function(time){
-    const minutes = Math.floor(time/60);
-    const seconds = Math.floor(time % 60);
-    return `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`
-};
-
-export { secondsToMinutes };
